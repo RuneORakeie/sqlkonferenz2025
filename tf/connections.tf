089ce23d67e9c4d3bb844dc34cf7470f5b3c1c2f
Rune Ovlien Rakeie <rune.rakeie@tietoevry.com>
2025-02-27 23:42:19 +0100

    Reset env

--- tf/connections.tf
+++ tf/connections.tf
@@ -36,37 +36,3 @@ module "ws_conn_eventhub" {
 
   ]
 }
-# terraform import module.ws_conn_eventhub.restapi_object.fabric_connection /connections/f29bed7c-a452-4329-b967-2c5a333a5bc9
-# terraform import 'module.ws_conn_eventhub.restapi_object.connection_role_assignment["0"]' /connections/f29bed7c-a452-4329-b967-2c5a333a5bc9/roleAssignments/65dc42b4-71be-4e0a-b1c2-11f961077e1d
-# terraform import 'module.ws_conn_eventhub.restapi_object.connection_role_assignment["1"]' /connections/f29bed7c-a452-4329-b967-2c5a333a5bc9/roleAssignments/5b2864ad-58d8-4442-a3e9-1c3d1a5cdca6
-# terraform import 'module.ws_conn_eventhub.restapi_object.connection_role_assignment["2"]' /connections/f29bed7c-a452-4329-b967-2c5a333a5bc9/roleAssignments/aa5edc36-080b-4b9c-bcee-3b95cabd902d
-/*
-{
-    "value": [
-        {
-            "id": "65dc42b4-71be-4e0a-b1c2-11f961077e1d",
-            "principal": {
-                "id": "65dc42b4-71be-4e0a-b1c2-11f961077e1d",
-                "type": "User"
-            },
-            "role": "Owner"
-        },
-        {
-            "id": "5b2864ad-58d8-4442-a3e9-1c3d1a5cdca6",
-            "principal": {
-                "id": "5b2864ad-58d8-4442-a3e9-1c3d1a5cdca6",
-                "type": "User"
-            },
-            "role": "User"
-        },
-        {
-            "id": "aa5edc36-080b-4b9c-bcee-3b95cabd902d",
-            "principal": {
-                "id": "aa5edc36-080b-4b9c-bcee-3b95cabd902d",
-                "type": "User"
-            },
-            "role": "User"
-        }
-    ]
-}
-*/
